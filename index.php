65d604bde6824e9d0a44d6e98df98845eddf52d9
Golga <golgarud@live.fr>
2017-11-09 15:12:32 +0100

    CORE html form sample

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>TestBulko - 2017</title>
+	<meta name="viewport" content="user-scalable=no, initial-scale = 1, minimum-scale = 1, maximum-scale = 1, width=device-width">
+	<link rel="icon" type="image/vnd.microsoft.icon" href="http://www.bulko.net/templates/img/favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="http://www.bulko.net/templates/img/favicon.ico" />
+	<link rel="stylesheet" href="https://cdn.bootcss.com/meyer-reset/2.0/reset.min.css">
+</head>
+<body>
+	<header>
+		<div class="title">TestBulko - 2017</div>
+		<div class="picto">
+			<a href="https://github.com/Bulko/test-dev-bulko" title="Lire les consignes">
+				<i class="fa fa-github">README.md</i>
+			</a>
+		</div>
+	</header>
+	<main>
+		<form method="post">
+			<div class="form-control">
+				<input type="text" name="nom" placeholder="Nom"/>
+			</div>
+			<div class="form-control">
+				<input type="email" name="email" placeholder="Email"/>
+			</div>
+			<div class="form-control">
+				<input type="tel" name="tel" placeholder="Téléphone"/>
+			</div>
+			<div class="form-control">
+				<textarea name="message" cols="30" rows="10"  placeholder="Message">
+					
+				</textarea>
+			</div>
+			<input type="submit" value="Envoyer" />
+		</form>
+	</main>
+	<footer>
+		🦄 GLHF
+	</footer>
+</body>
+</html>
